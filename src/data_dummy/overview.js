40749e3d18f9c1c327ca1616a2951cb6802e9964
Sulistyo Ardani <sulistyoardani@gmail.com>
2020-12-22 01:05:25 +0700

    sulis - banner detail movie

---
+++ src/data_dummy/overview.js
@@ -0,0 +1,19 @@
+import IconCarousel from 'assets/images/carousel.svg'
+export const Overviewx =
+  {
+    sinposis:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, cupiditate iusto at tempore accusantium deleniti fugiat aspernatur numquam corporis, animi dignissimos quo velit pariatur obcaecati officia debitis perferendis perspiciatis harum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, cupiditate iusto at tempore accusantium deleniti fugiat aspernatur numquam corporis, animi dignissimos quo velit pariatur obcaecati officia debitis perferendis perspiciatis harum?",
+    movie_info: {
+      release_data: "January 13 1996",
+      director: "Jhon Doe",
+      featured_song: "Pegasus Fantasy",
+      budget: "200 Milion USD",
+    },
+    banner: IconCarousel,
+    rating: 10,
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, cupiditate iusto at tempore accusantium deleniti fugiat aspernatur numquam corporis, animi dignissimos quo velit pariatur obcaecati officia debitis perferendis perspiciatis harum?",
+    review_count : 2200,
+    title: "SAINT SEIYA"
+  }
+;
