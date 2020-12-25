642e05bfc28e5921d26437d2f30321547ae84aaa
Sulistyo Ardani <sulistyoardani@gmail.com>
2020-12-26 05:08:58 +0700

    sulis - homepage - reveal on scroll

--- src/components/Homepage/card.js
+++ src/components/Homepage/card.js
@@ -1,6 +1,11 @@
-import React, {useState, useContext, useEffect} from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Homepage } from "data_dummy/homepage";
 import Category from "components/Homepage/category";
+import Slide from "react-reveal/Slide";
+// import Reveal from "react-reveal/Slide";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import {
   TabContent,
   TabPane,
@@ -16,98 +21,97 @@ import {
 } from "reactstrap";
 import classnames from "classnames";
 import Styled from "styled-components";
-import {darkMode} from "provider/darkmode"
+import { darkMode } from "provider/darkmode";
 import { act } from "@testing-library/react";
 const Cardx = (props) => {
-
-    const {
-      activeTab,
-      setActiveTab,
-      toggle,
-    } = useContext(darkMode);
-    console.log(activeTab, "akitp")
+  AOS.init();
+  const { activeTab, setActiveTab, toggle } = useContext(darkMode);
+  console.log(activeTab, "akitp");
   const valueCategory = [
     {
       name: "Thriller",
       spacing: "mr-3",
       page: "/characters",
-      id: 1
+      id: 1,
     },
     {
       name: "Comedy",
       spacing: "mr-3",
       page: "/review",
-      id: 2
+      id: 2,
     },
     {
       name: "Korea",
       spacing: "mr-3",
       page: "/overview",
-      id: 3
+      id: 3,
     },
     {
       name: "Si-Fi",
       spacing: "mr-3",
       page: "/characters",
-      id: 4
+      id: 4,
     },
     {
       name: "Action",
       spacing: "mr-3",
       page: "/review",
-      id: 5
+      id: 5,
     },
   ];
-  const [pell, setPell] = useState("1")
+  const [pell, setPell] = useState("1");
+  const [delay, setDelay] = useState(2000);
   useEffect(() => {
     // if(pell !== activeTab) {
     //   setPell(activeTab);
     // }
-    setPell(activeTab)
+    setPell(activeTab);
   }, [activeTab]);
   console.log(pell, activeTab, "banding");
+  let z = 300;
+  const Loopx = Homepage.map((val, i) => {
+    if (i !== 0 && z <= 1500) {
+      z += 300;
+    } else {
+      z = 300;
+    }
+    //map, foreach, while, do while, for
+    return (
+      <>
+        <div className="col-auto m-md-0 mx-auto ">
+          {/* <Slide bottom duration={z}> */}
+          <img
+            className="cardx"
+            src={val.image}
+            data-aos="fade-up"
+            data-aos-delay={(300 + z).toString()}
+            data-aos-duration={(300 + z).toString()}
+          />
+          <p style={({ color: "white" }, { fontSize: "20px" })}>{val.title}</p>
+          <p style={{ marginTop: "-25px" }}>{val.category}</p>
+        </div>
+      </>
+    );
+  });
   return (
     <>
       <Category valueProps={valueCategory} detect={"homepage"} />
-    {console.log(pell, activeTab, "active tab")}
+      {console.log(pell, activeTab, "active tab")}
       <TabContent activeTab={pell}>
         <TabPane tabId="1">
-          <div className="d-flex flex-wrap mx-auto">
-            {Homepage.map((val) => {
-              //map, foreach, while, do while, for
-              return (
-                <>
-                  <div className="col-auto m-md-0 mx-auto ">
-                    <img className="cardx" src={val.image} />
-                    <p style={({ color: "white" }, { fontSize: "20px" })}>
-                      {val.title}
-                    </p>
-                    <p style={{ marginTop: "-25px" }}>{val.category}</p>
-                  </div>
-                </>
-              );
-            })}
-          </div>
+          <div className="d-flex flex-wrap mx-auto">{Loopx}</div>
         </TabPane>
         <TabPane tabId="2">
-          <div className="d-flex flex-wrap mx-auto">
-            "2"
-          </div>
+          <div className="d-flex flex-wrap mx-auto">"2"</div>
         </TabPane>
         <TabPane tabId="3">
-          <div className="d-flex flex-wrap mx-auto">
-            "3"
-          </div>
+          <div className="d-flex flex-wrap mx-auto">"3"</div>
         </TabPane>
         <TabPane tabId="4">
-          <div className="d-flex flex-wrap mx-auto">
-            "4"
-          </div>
+          <div className="d-flex flex-wrap mx-auto">"4"</div>
         </TabPane>
         <TabPane tabId="5">
-          <div className="d-flex flex-wrap mx-auto">
-            "5"
-          </div>
+          <div className="d-flex flex-wrap mx-auto">"5"</div>
         </TabPane>
       </TabContent>
     </>
